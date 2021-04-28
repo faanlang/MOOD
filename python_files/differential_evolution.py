a4ad9b13ed33967458ef801044f6bf87cf9783a8
Faan Langelaan <faanlangelaan@gmail.com>
2021-04-28 21:20:31 +0200

    DE working

--- python_files/differential_evolution.py
+++ python_files/differential_evolution.py
@@ -5,16 +5,26 @@ Created on Wed Apr 21 13:55:41 2021
 @author: swlon
 """
 import numpy as np 
+import matplotlib.pyplot as plt
 from numpy import random
+from numpy.random import default_rng
 from classconfig_file import config
-#from dominance_filter_file import dominance_filter
+from dominance_filter_file import dominance_filter
+
 x = config
+rng = default_rng()
 
 def differential_evolution(config):
     Nobj = config.NOBJ
     Nvar = config.NVAR
-    Xpop = config.Xpop
+    pop_size = config.Xpop
+    sub_pop_size = config.subXpop
+    generations = config.MAXGEN
     initial_bounds = config.initial_bounds
+    
+    scaling_factor = config.scaling_factor
+    crossover_rate = config.CR_rate
+    
     def evaluation(f):
         return config.cost_function(f)
     
@@ -22,48 +32,127 @@ def differential_evolution(config):
     
      #Population initialization
     
-    parent        = np.zeros((Xpop, Nvar))
-    J_parent = np.zeros((Xpop, Nobj))
+    parent   = np.zeros((pop_size, Nvar))
+    j_parent = np.zeros((pop_size, Nobj))
 
-    for xpop in range(0,Xpop): #Uniform distribution
+    for pop_member in range(0,pop_size): 
         for variable in range(0,Nvar):
-            parent[xpop,variable] = initial_bounds[variable][0] +\
+            parent[pop_member,variable] = initial_bounds[variable][0] +\
             (initial_bounds[variable][1] - initial_bounds[variable][0])*random.rand()
    
     
-    for xpop in range(0,Xpop): #Uniform distribution
-        J_parent[xpop] = evaluation(parent[xpop])
+    for pop_member in range(0,pop_size): 
+        j_parent[pop_member] = evaluation(parent[pop_member])
     
     
- 
     
-
-    FES    = Xpop      # Function Evaluations
-    PFront = J_parent # The approximated Pareto Front
+    PFront = j_parent # The approximated Pareto Front
     PSet   = parent    # The approximated Pareto Set
     
-    print(PFront)
-    print(PFront[0])
+    #print(PFront)
+   
+    #plt.scatter(PFront[:,0],PFront[:,1],linewidth=0.2,marker = "o", facecolor = 'none',edgecolor = 'darkorange',  label = 'A -')
+    PFront, PSet = dominance_filter(PFront,PSet,x)
+    #print(len(PFront))
+    #plt.scatter(PFront[:,0],PFront[:,1])
+    
+            
 
-# if size(spMODEDat.PobInitial,1) >= 1 % If we include members in the Pop.
-#     InitialPS = spMODEDat.PobInitial;
-#     InitialPF = mop(InitialPS,spMODEDat);
-#     FES=FES+size(spMODEDat.PobInitial,1);
-#     [PFront PSet spMODEDat] = ...
-#         Dominance([PFront;InitialPF],[PSet;InitialPS],spMODEDat);
-#             % Calling Dominance Filter
-# else
-#     [PFront PSet spMODEDat]=Dominance(PFront,PSet,spMODEDat);
-#             %Calling Dominance Filter
-# end
-
-# if size(PFront,1)>1
-#     spMODEDat.ExtremesPFront=[max(PFront);min(PFront)]; % Extremes
-# end
-
-# [AnchorsY, AnchorsX] = Look4Anchors(PFront,PSet);       % Anchor points
-# spMODEDat.AnchorsY = AnchorsY;
-# spMODEDat.AnchorsX = AnchorsX;
+    for n in range(0,generations):
+        
+        #create subpopulation
+        
+        #making sure the size of the subpopulation is not greater then the current PSet
+        if len(PSet)<= sub_pop_size:
+            sub_pop_size = len(PSet)
+            sub_parent = PSet
+            j_sub_parent = PFront
+            sub_smaller = True
+        # if this is not the case, take a rdm subset of the PSet
+        else:
+            sub_smaller = False
+            sub_parent = np.zeros((sub_pop_size,Nvar))
+            j_sub_parent = np.zeros((sub_pop_size,Nobj))
+            
+            numbers = rng.choice(len(PSet), size=sub_pop_size, replace=False)
+            for c in range(0,sub_pop_size):
+                sub_parent[c] = PSet[numbers[c]]
+                j_sub_parent[c] = PFront[numbers[c]]
+        
+        
+        
+        mutant   = np.zeros((sub_pop_size, Nvar))
+        child = np.zeros((sub_pop_size, Nvar))
+        j_child = np.zeros((sub_pop_size, Nobj))
+        
+        for sub_member in range(0,sub_pop_size):
+            #find 3 indexes not the same as target vector
+            idxs = [idx for idx in range(sub_pop_size) if idx != sub_member] # (j = 0) 
+            selected = np.random.choice(idxs, 3, replace=False)
+            a,b,c = sub_parent[selected]
+            #make mutant based of 3 other vectors
+            mutant[sub_member] = a + scaling_factor*(b-c)
+            
+            
+            #making sure the mutant does not exceed variable bounds
+            for m_variable in range (0,Nvar):
+                if mutant[sub_member,m_variable]< initial_bounds[m_variable][0]:
+                    mutant[sub_member,m_variable]= initial_bounds[m_variable][0]
+                elif mutant[sub_member,m_variable]> initial_bounds[m_variable][1]:
+                    mutant[sub_member,m_variable]= initial_bounds[m_variable][1]
+            
+            #make the child based on parent and mutant
+            cross_points = np.random.rand(Nvar) < crossover_rate
+            #make sure there is at least one mutation
+            if not np.any(cross_points):
+                cross_points[np.random.randint(0, Nvar)] = True
+                
+            child[sub_member] = np.where(cross_points, mutant[sub_member], sub_parent[sub_member])
+            
+            j_child[sub_member] = evaluation(child[sub_member])
+            
+            # tracker = 0
+            # for c_variable in range (0,Nvar):
+            #     if j_child[sub_member,c_variable]<= j_sub_parent[sub_member,c_variable]:
+            #         tracker = tracker +1
+            # if tracker == Nobj:
+                    
+                    
+                
+    # for subxpop=1:SubXpop# Pure dominance is used for selection process.
+    #     if JxChild(subxpop,:) <= JxSubParent(subxpop,:)
+    #         SubParent(subxpop,:) = Child(subxpop,:);
+    #         JxSubParent(subxpop,:) = JxChild(subxpop,:);
+    #     end
+                
+                
+        #add all childs to population and run dominance filter
+        # PFront8 =np.array(PFront)
+        # PSet8 =np.array(PSet)
+        # j_child8 =np.array(j_child)
+        # child8 =np.array(child)
+        
+        
+        big_front = np.concatenate((PFront, j_child))
+        big_set = np.concatenate((PSet, child))
+        
+        #plot before update
+        plt.figure()
+        plt.scatter(PFront[:,0],PFront[:,1],color = 'darkorange')
+    
+    #update PFront, Pset
+        PFront, PSet = dominance_filter(big_front,big_set,x)
+        
+        #plot after update
+        plt.scatter(PFront[:,0],PFront[:,1])
+        plt.ylim(0,20)
+        plt.xlim(0,0.2)
+        
+        plt.show()
+        print(len(PFront))
+        
+        
+        
     
     
     
@@ -71,10 +160,6 @@ def differential_evolution(config):
     
     
     
-    
-    
-    
-    print(parent)
-    print(Nobj)
-    
+    #print(parent)
+    #print(Nobj)  
 differential_evolution(x)
--- python_files/dominance_filter_file.py
+++ python_files/dominance_filter_file.py
@@ -4,17 +4,17 @@ Created on Wed Apr 21 16:41:41 2021
 
 @author: swlon
 """
-
+#KNOWN BUG: FILTERS OUT 2 OF THE SAME NON-DOMINATED SOLUTIONS
 import numpy as np
 
-a = [[1,2],[1,5],[2,4],[3,3],[4,1],[2,1]]
-b = [[1,2],[1,5],[2,4],[3,3],[4,1],[2,1]]
+# a = [[2,2],[1,5],[2,4],[4,1],[2,1],[3,3],[2,2]]
+# b = [[55,2],[1,5],[2,4],[3,3],[4,1],[2,1],[1,1],[0,0],[2,2]]
 
 class vb:
     NOBJ = 2
     NVAR = 2
 
-#
+#   1
 
 
 def dominance_filter(j_front, x_set,config):
@@ -27,31 +27,38 @@ def dominance_filter(j_front, x_set,config):
     
     
     for j in range(0,population_size):
-        dominated = True
+        dominated = False
         
         for compare in range(0,population_size):
+            tracker = 0    
+            tracker2 = 0
             if j !=  compare:
+                
                 for objective in range(0,Nobj):
-                    if j_front[j][objective] > j_front[compare][objective]:
-                        break
-                    if objective == Nobj -1:
-                        dominated = False
-                            #print(j_front[j],j_front[compare])
-                            #print('dominated')
-        
+                    
+                    if j_front[compare][objective] <= j_front[j][objective]:
+                        tracker = tracker + 1
+                    if j_front[compare][objective] == j_front[j][objective]:
+                        tracker2 = tracker2 + 1
+                    
+                    if tracker == Nobj:
+                        dominated = True
+                        if tracker2 ==Nobj:
+                            print('potential error')
+                            break
+                
+                
         if dominated == False:
-            
-            
-            
             PFront[k] = j_front[j]
             PSet[k] = x_set[j]
             k = k+1
                     
     PFront = PFront[:k]
     PSet = PSet[:k]
-                
+    
+            
     
     return(PFront,PSet)
 
-print(dominance_filter(a,b,vb))
+#print(dominance_filter(a,b,vb))
 
